82a2fa293713b82565e34318ad53ccb38b0939d5
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-03-31 12:36:40 +0200

    [ADD] '<<' Command

--- src/parsing/change_fd.c
+++ src/parsing/change_fd.c
@@ -26,20 +26,42 @@ static void get_file(char *file, char *line, int start, char chevrons[2])
     my_memset(&line[start], ' ', count);
 }
 
+static int here_document_command(char const *end)
+{
+    char *line = NULL;
+    int pipefd[2];
+
+    if (pipe(pipefd) != 0)
+        return (-1);
+    while (my_putstr("? ") &&
+    get_next_line(&line, 0) && my_strcmp(line, end) != 0) {
+        my_putstr_fd(pipefd[1], line);
+        my_putstr_fd(pipefd[1], "\n");
+    }
+    close(pipefd[1]);
+    if (line != NULL)
+        free(line);
+    return (pipefd[0]);
+}
+
 int get_input_fd(char *line)
 {
     char chevrons[2] = {'<', '>'};
     char file[256];
     int fd = 0;
     int chevron = my_strchr_index(line, chevrons[0]);
+    bool here_document = false;
 
     if (chevron < 0)
         return (0);
+    here_document = (line[chevron + 1] == chevrons[0]);
     get_file(file, line, chevron, chevrons);
     if (my_strlen(file) == 0) {
         my_putstr_error("Missing name for redirect.\n");
         return (-1);
     }
+    if (here_document)
+        return (here_document_command(file));
     fd = open(file, O_RDONLY);
     if (fd == -1)
         print_error(file, strerror(errno));
--- tests/test_redirections_stdin.c
+++ tests/test_redirections_stdin.c
@@ -24,6 +24,38 @@ Test(stdin_redirection, read_a_file_as_standard_input)
     cr_expect_stdout_eq_str("Yoo$\n");
 }
 
+static void print_on_fd_for_stdin_test(int fd)
+{
+    my_putstr_fd(fd, "Bonjour\n");
+    my_putstr_fd(fd, "C'est moi\n");
+    my_putstr_fd(fd, "Sans EOF\n");
+    my_putstr_fd(fd, "EOF\n");
+}
+
+Test(stdin_redirection, handle_here_document_redirection)
+{
+    char **envp = my_array_dup(DEFAULT_ENVIRONMENT);
+    int pipefd[2];
+    int child = 0;
+
+    cr_assert_eq(pipe(pipefd), 0);
+    child = fork();
+    if (child == 0) {
+        close(pipefd[0]);
+        print_on_fd_for_stdin_test(pipefd[1]);
+        close(pipefd[1]);
+        exit(0);
+    }
+    close(pipefd[1]);
+    dup2(pipefd[0], 0);
+    waitpid(child, NULL, 0);
+    cr_redirect_stdout();
+    cr_expect_eq(minishell("cat << EOF", &envp), 0);
+    my_free_array(envp);
+    cr_expect_stdout_eq_str("? ? ? ? " "Bonjour\nC'est moi\nSans EOF\n");
+    close(pipefd[0]);
+}
+
 Test(stdin_redirection, print_error_if_there_is_no_file)
 {
     char **envp = my_array_dup(DEFAULT_ENVIRONMENT);
